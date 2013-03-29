7f37e591cee5e1affa3499b2c4d99b2da29a8d94
John-Patrik Nilsson <john-patrik@codeleaf.se>
2013-03-30 00:06:02 +0100

    initial commit, with rvmrc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JohnPatrik::Application.config.secret_token = 'c29ee6ed7e1e3196502bf83ee3cd3968aeb14c5a1d68eb2ae58d5554b9adcb89f781cc2740b91c289cc4cc9942f5dd8335b1bea124f9238fcf4a050b8305fbd1'
